8d33efbf33d564ac323873d421e2e4c9672854a5
sannidhsinghal <49746384+sannidhsinghal@users.noreply.github.com>
2019-04-22 00:02:37 +0530

    1
    1

---
+++ src/one/Mainapp.java
@@ -0,0 +1,14 @@
+package one;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Mainapp {
+	public static void main(String args[])
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext("Bean.xml");
+	      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+	      obj.getMessage();
+	}
+
+}
